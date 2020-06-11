9ef03b0c84d59d85ee73752bcb097cc82651274c
charromax <manuelrg88@gmail.com>
2020-06-11 20:53:55 -0300

    detalles

--- practica3ej1.py
+++ practica3ej1.py
@@ -4,10 +4,12 @@
 # ---------------------------------------------------------------------------------
 
 ene = int(input("Cual es el valor de N? -> "))
+suma= 0
 
 for numeros in range(ene): # la variable numeros va adquiriendo valores entre 0 y N con cada iteracion
     # numeros = int(input("ingrese numero: ")) <- tambien podria pedir al usuario cada numero pero directamente voy a tomar el indice como input
     suma += numeros        # esto equivale a decir suma = suma + numeros - con cada vuelta va agregando el valor de cada numero
 
 promedio = suma / ene
-print(f"El promedio de los {ene} numeros es {promedio}")
+
+print("El promedio de los %s numeros es %s" % (ene, promedio)) # otra forma de construir una string con variables
