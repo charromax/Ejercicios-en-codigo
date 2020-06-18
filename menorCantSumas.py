b2f403bf6e34ff93955aa44db3b2f17031a62ecc
charromax <manuelrg88@gmail.com>
2020-06-17 21:06:22 -0300

    ahora si bien

--- menorCantSumas.py
+++ menorCantSumas.py
@@ -1,7 +1,8 @@
 import random
 
-listaDelUnoAlCien = [*range(1, 100)]
-  # genera una lista de 100 numeros ordenados entre 0 y 100
+randomlist = []
+for i in range(0, 100):
+    randomlist.append(random.randint(0, 100))  # genera una lista de 50 numeros aleatorios entre 0 y 110
 
 # --------------------------------------------------------------------------------------
 
@@ -9,10 +10,12 @@ cantidadDeSumas = 0
 suma = 0
 sumaString = ''
 
-while suma < 55:
+for num in randomlist:
+    suma += num
+    sumaString += (str(randomlist[cantidadDeSumas]) + " + ")
     cantidadDeSumas = cantidadDeSumas + 1
-    suma += listaDelUnoAlCien[cantidadDeSumas]
-    sumaString += (str(cantidadDeSumas) + " + ")
+    if suma > 55:
+        break
 
 print(f"la suma es {sumaString}")
 print(f"la menor cantidad de sumas para llegar a 55 es: {cantidadDeSumas}")
